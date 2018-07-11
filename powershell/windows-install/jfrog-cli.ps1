5c90634f7a758e52d8c90b162cfdb955d4748d7b
George Cave <gcave@stablecoder.ca>
2018-07-11 07:27:19 -0400

    Added powershell script for JFrog CLI

---
+++ powershell/windows-install/jfrog-cli.ps1
@@ -0,0 +1,5 @@
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+wget https://bintray.com/jfrog/jfrog-cli-go/download_file?file_path=1.17.1%2Fjfrog-cli-windows-amd64%2Fjfrog.exe -OutFile jfrog.exe -UseBasicParsing
+mkdir C:\Program Files\JFrog
+Move-Item .\jfrog.exe -Destination "C:\Program Files\JFrog\jfrog.exe"
+[Environment]::SetEnvironmentVariable( "Path", [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";C:\Program Files\JFrog;", [System.EnvironmentVariableTarget]::Machine )
