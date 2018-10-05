a82a4013c9c6f0ad8bab72718fa41fe26a94644f
George Cave <gcave@stablecoder.ca>
2018-10-04 21:39:18 -0400

    Updated versions of windows items

--- powershell/windows-install/latest_versions.ps1
+++ powershell/windows-install/latest_versions.ps1
@@ -1,6 +1,6 @@
 $env:GLFW_VER = "3.2.1"
-$env:GLM_VER = "0.9.9.0"
+$env:GLM_VER = "0.9.9.2"
 $env:PORTAUDIO_VER = "190600_20161030"
-$env:VULKAN_SDK_VER = "1.1.82.0"
+$env:VULKAN_SDK_VER = "1.1.85.0"
 $env:BULLET_VER = "2.87"
 $env:CATCH_VER = "2.3.0"
