c11f456a5645fc456c0be53c5c59676d5f0b5292
George Cave <gcave@stablecoder.ca>
2018-12-08 17:26:59 -0500

    Some fun scripts from the internet

---
+++ bash/fun/much_compute.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo '                     much compute
+               Y.                     _
+               YiL                  .```.
+               Yii;                ; .;; .
+ WOW           YY;ii._            : .;;;; :
+               iiYYYYYYYiiiii;;;;i  ;;::;;;;
+           _.;YYYYYYYiiiiiiYYYii  .;;.   ;;;    very controller
+        .YYYYYYYYYYiiYYYYYYYYYYYYYii;   ;;;;
+      .YYYYYYYYYYiiYYYYYYYYYYYYYYYYY;.ii;`..
+     :YYYYYY$$YYiiYY$$$$iiiYYYYYYYiiYYYYiYYii.
+    :YYY$!. TYiiYY$$$$$YYYYYYYYYYYiiiYYYYiYY.
+    Y$MM$:   :YYYYYY$!"``"4YYYYYYiiiYYYYiiYY.
+ .  :MM$$b.,dYY$$Yii" :"   :YYYYllYiiYYYiYY
+_ :`4MM$!YYYYYYYYYii,.__.diii$$YYYYYYYYYYYY
+_ $b`P`     "$$$$$$iiiiiiii$$$$YY$$$$$$YiY;   such done
+ ,.`$:       :$$$$$$$$$YYYYY$$$$$$$$$YYiiYYL
+ "`;$$.    .;PPb$`.,.``T$$YY$$$$YYYYYYiiiYYU
+ ;$P$;;; ;;;;i$y$"!Y$$$b;$$$Y$YY$$YYYiiiYYiY
+ $Fi$$ ..  ``iii.`-";YYYYY$$YY$$$$$YYYiYiYYY
+ :Y$$rb ````  `_..;;i;YYY$YY$$$$$$$YYYYYYiYY:
+  :$$$$$i;;iiiiidYYYYYYYYYY$$$$$$YYYYYYiiYYYY.
+   `$$$$$$$YYYYYYYYYYYYY$$$$$$YYYYYYiiiYYYYYYY
+   .i!$$$$$$YYYYYYYY$$$$$$YYY$$YYiiiiiiYYYYYYY
+
+         very success
+                                   so internet'
