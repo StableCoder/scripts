9a1839e3e614021b1dbd21ca1d1fc2c95ba9dc95
George Cave <gcave@stablecoder.ca>
2019-03-12 07:33:55 -0400

    Updated libyaml powershell script
    Now also adds the library directory to environment variable, and does
    its work in a libyaml subdirectory for isolation.

--- powershell/windows-install/libyaml.ps1
+++ powershell/windows-install/libyaml.ps1
@@ -1,22 +1,23 @@
-try{
-[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-wget https://github.com/yaml/libyaml/archive/${env:LIBYAML_VER}.tar.gz -OutFile libyaml.tar.gz -UseBasicParsing
-7z x -aoa libyaml.tar.gz
-Remove-Item -path libyaml.tar.gz
-7z x -aoa libyaml.tar
-Remove-Item -path libyaml.tar
-cd libyaml-${env:LIBYAML_VER}
-mkdir build; cd build
-cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="C:\libyaml" -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF
-ninja
-ninja install
-cd ../..
-Remove-Item -path libyaml-${env:LIBYAML_VER} -Recurse -ErrorAction SilentlyContinue
+try {
+    mkdir libyaml
+    cd libyaml
+    [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+    wget https://github.com/yaml/libyaml/archive/${env:LIBYAML_VER}.tar.gz -OutFile libyaml.tar.gz -UseBasicParsing
+    7z x -aoa libyaml.tar.gz
+    Remove-Item -path libyaml.tar.gz
+    7z x -aoa libyaml.tar
+    Remove-Item -path libyaml.tar
+    cd libyaml-${env:LIBYAML_VER}
+    mkdir build; cd build
+    cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="C:\libyaml" -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF
+    ninja
+    ninja install
+    cd ../../..
+    Remove-Item -path .\libyaml\ -Recurse -ErrorAction SilentlyContinue
 
-[Environment]::SetEnvironmentVariable( "PATH", [System.Environment]::GetEnvironmentVariable("PATH","Machine") + ";C:\libyaml\bin", [System.EnvironmentVariableTarget]::Machine )
-[Environment]::SetEnvironmentVariable( "CUSTOM_INCLUDE", [System.Environment]::GetEnvironmentVariable("CUSTOM_INCLUDE","Machine") + ";C:\libyaml\include", [System.EnvironmentVariableTarget]::Machine )
-}
-catch
-{
+    [Environment]::SetEnvironmentVariable( "PATH", [System.Environment]::GetEnvironmentVariable("PATH","Machine") + ";C:\libyaml\bin", [System.EnvironmentVariableTarget]::Machine )
+    [Environment]::SetEnvironmentVariable( "CUSTOM_INCLUDE", [System.Environment]::GetEnvironmentVariable("CUSTOM_INCLUDE","Machine") + ";C:\libyaml\include", [System.EnvironmentVariableTarget]::Machine )
+    [Environment]::SetEnvironmentVariable( "CUSTOM_LIB", [System.Environment]::GetEnvironmentVariable("CUSTOM_LIB","Machine") + ";C:\libyaml\lib", [System.EnvironmentVariableTarget]::Machine )
+} catch {
     exit 1
 }
