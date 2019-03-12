17b4d70bc47c71e002fd056946eb2c676baa2014
George Cave <gcave@stablecoder.ca>
2019-03-12 08:24:32 -0400

    Updated newton install script
    Now uses latest master instead of the last stable version, as the last
    stable version did not link properly.

--- powershell/windows-install/newton.ps1
+++ powershell/windows-install/newton.ps1
@@ -1,15 +1,13 @@
 try {
     mkdir newton; cd newton
     [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-    wget https://github.com/MADEAPPS/newton-dynamics/archive/newton-${env:NEWTON_VER}.tar.gz -OutFile newton.tar.gz -UseBasicParsing
-    7z x newton.tar.gz
-    Remove-Item -path newton.tar.gz
-    7z x newton.tar
-    Remove-Item -path newton.tar
-    cd newton-dynamics-newton-${env:newton_VER}
+    wget https://github.com/MADEAPPS/newton-dynamics/archive/master.zip -OutFile newton.zip -UseBasicParsing
+    7z x newton.zip
+    cd newton-dynamics-master
     mkdir build; cd build
-    cmake .. -G "Visual Studio 15 2017 Win64"  -DNEWTON_BUILD_SANDBOX_DEMOS=OFF
-    cmake --build . --config Release --target install
+    cmake .. -G Ninja -DNEWTON_BUILD_SANDBOX_DEMOS=OFF -DCMAKE_BUILD_TYPE=Release
+    ninja
+    ninja install
     mkdir C:\newton
     Copy-Item build\* -Destination C:\newton\ -Recurse
     cd ../../..
@@ -18,8 +16,6 @@ try {
     [Environment]::SetEnvironmentVariable( "PATH", [System.Environment]::GetEnvironmentVariable("PATH","Machine") + ";C:\newton\bin", [System.EnvironmentVariableTarget]::Machine )
     [Environment]::SetEnvironmentVariable( "CUSTOM_INCLUDE", [System.Environment]::GetEnvironmentVariable("CUSTOM_INCLUDE","Machine") + ";C:\newton\include", [System.EnvironmentVariableTarget]::Machine )
     [Environment]::SetEnvironmentVariable( "CUSTOM_LIB", [System.Environment]::GetEnvironmentVariable("CUSTOM_LIB","Machine") + ";C:\newton\lib", [System.EnvironmentVariableTarget]::Machine )
-}
-catch
-{
+} catch {
     exit 1
 }
