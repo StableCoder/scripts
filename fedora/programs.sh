d4bdb0a6955e9fdebf9c6ddcb6c10115d0bec5aa
George Cave <gcave@stablecoder.ca>
2019-09-16 22:31:52 -0400

    Added missing VSCode install command

--- fedora/programs.sh
+++ fedora/programs.sh
@@ -14,9 +14,11 @@ dnf install -y git git-lfs subversion make ninja-build cmake doxygen graphviz
 dnf install -y libasan liblsan libtsan libubsan clang-tools-extra iwyu cppcheck
 ## Editors
 dnf install -y vim kate
+
 ## VS Code
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf install -y code
 
 # Virtualization
 dnf install -y docker libvirt virt-manager qemu qemu-kvm docker-compose qemu-block-gluster gluster-fs
