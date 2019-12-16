d9eac9bac95c5a568391bda499e640220f2cb1e7
George Cave <gcave@stablecoder.ca>
2019-12-16 10:12:49 -0500

    Updated macOS install script for VulkanSDK
    Grabbing the latest, always, and installing/clearing the directory
    after.

--- mac/install.sh
+++ mac/install.sh
@@ -7,5 +7,9 @@
 brew install git-lfs cmake ninja
 git lfs install
 
-# Conan
-pip3 install conan
+# Vulkan
+curl -O https://sdk.lunarg.com/sdk/download/latest/mac/vulkan-sdk.tar.gz
+cd vulkansdk-macos-*
+./install-vulkan.py
+cd ..
+rm -rf vulkan*
