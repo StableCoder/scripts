c41c4d3d1fa831950409827a60d98c3eae302201
George Cave <gcave@stablecoder.ca>
2020-10-18 14:53:14 -0400

    Updated looking-glass script for sudo/user mode

--- linux-install/arch-looking-glass.sh
+++ linux-install/arch-looking-glass.sh
@@ -1,6 +1,16 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+set -e
 
-yay -S --noconfirm --nodiffmenu --noeditmenu --noupgrademenu --afterclean looking-glass obs-plugin-looking-glass-git
+RED='\033[0;31m'
+DEFAULT='\033[0m'
+
+if [ $SUDO_USER ]; then user=$SUDO_USER; else user=$(whoami); fi
+if [ "$user" == "root" ] || [ "$user" == "" ]; then
+    echo -e "${RED}Don't run this as root! Create a user and run this as sudo.${DEFAULT}"
+    exit 1
+fi
+
+runuser -l $user -c 'yay -S --noconfirm --nodiffmenu --noeditmenu --noupgrademenu --afterclean looking-glass obs-plugin-looking-glass-git'
 
 cat >/usr/local/bin/init-looking-glass-file.sh <<EOF
 #!/usr/bin/env sh
