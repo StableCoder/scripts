699b310eaf8413fea9e607f9fbf8121d0a141833
George Cave <gcave@stablecoder.ca>
2020-11-13 11:41:53 -0500

    Updated VulkanSDK Windows script
    Rather than specifying the particular VulkanSDK version, it now will
    pull the 'latest' available.

--- powershell/windows-install/vulkan-sdk.ps1
+++ powershell/windows-install/vulkan-sdk.ps1
@@ -1,8 +1,9 @@
 try {
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
-wget https://sdk.lunarg.com/sdk/download/$env:VULKAN_SDK_VER/windows/VulkanSDK-$env:VULKAN_SDK_VER-Installer.exe?Human=true -OutFile VulkanSDK.exe -UseBasicParsing
+wget https://sdk.lunarg.com/sdk/download/latest/windows/vulkan-sdk.exe?Human=true -OutFile VulkanSDK.exe -UseBasicParsing
 .\VulkanSDK.exe /S /D=C:\VulkanSDK | Out-Null
 Remove-Item VulkanSDK.exe
+Remove-Item -Path C:\VulkanSDK\Bin32 -Recurse -ErrorAction SilentlyContinue
 
 [Environment]::SetEnvironmentVariable( "CUSTOM_INCLUDE", [System.Environment]::GetEnvironmentVariable("CUSTOM_INCLUDE","Machine") + ";C:\VulkanSDK\Include", [System.EnvironmentVariableTarget]::Machine )
 [Environment]::SetEnvironmentVariable( "CUSTOM_LIB", [System.Environment]::GetEnvironmentVariable("CUSTOM_LIB","Machine") + ";C:\VulkanSDK\lib", [System.EnvironmentVariableTarget]::Machine )
