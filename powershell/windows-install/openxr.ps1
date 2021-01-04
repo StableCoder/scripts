57779e460cc4f6a6a71d74bc55ca256d474745d9
George Cave <gcave@stablecoder.ca>
2021-01-04 15:32:52 -0500

    Added new paths for OpenXR windows builds
    For debug builds of OpenXR, both of the binaries are in a subdirectory,
    so the updated PATH environment variables now also add this
    subdirectory.

--- powershell/windows-install/openxr.ps1
+++ powershell/windows-install/openxr.ps1
@@ -42,7 +42,7 @@ try {
     # Setup the environment variables (Only if not found in the var already)
     if($null -eq ( ";C:\\openxr-sdk\\bin" | ? { [System.Environment]::GetEnvironmentVariable("PATH","Machine") -match $_ })) {
         # PATH
-        [Environment]::SetEnvironmentVariable( "PATH", [System.Environment]::GetEnvironmentVariable("PATH","Machine") + ";C:\openxr-sdk\bin", [System.EnvironmentVariableTarget]::Machine )
+        [Environment]::SetEnvironmentVariable( "PATH", [System.Environment]::GetEnvironmentVariable("PATH","Machine") + ";C:\openxr-sdk\bin;C:\openxr-sdk\bin\Debug", [System.EnvironmentVariableTarget]::Machine )
     }
     if($null -eq ( ";C:\\openxr-sdk\\include" | ? { [System.Environment]::GetEnvironmentVariable("CUSTOM_INCLUDE","Machine") -match $_ })) {
         # CUSTOM_INCLUDE
@@ -50,7 +50,7 @@ try {
     }
     if($null -eq ( ";C:\\openxr-sdk\\lib" | ? { [System.Environment]::GetEnvironmentVariable("CUSTOM_LIB","Machine") -match $_ })) {
         # CUSTOM_LIB
-        [Environment]::SetEnvironmentVariable( "CUSTOM_LIB", [System.Environment]::GetEnvironmentVariable("CUSTOM_LIB","Machine") + ";C:\openxr-sdk\lib", [System.EnvironmentVariableTarget]::Machine )
+        [Environment]::SetEnvironmentVariable( "CUSTOM_LIB", [System.Environment]::GetEnvironmentVariable("CUSTOM_LIB","Machine") + ";C:\openxr-sdk\lib;C:\openxr-sdk\lib\Debug", [System.EnvironmentVariableTarget]::Machine )
     }
 } catch {
     # Cleanup the failed build folder
