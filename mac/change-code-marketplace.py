c12196cab410c894289f3b8980fb36ae744262e3
George Cave <gcave@stablecoder.ca>
2021-02-18 18:02:20 -0500

    Added VSCodium marketplace change for macOS
    Changes from the default marketplace to Microsoft's one, which has more
    selection.

---
+++ mac/change-code-marketplace.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import json
+
+# This works at least for macOS VSCodium
+file_path = "/Applications/VSCodium.app/Contents/Resources/app/product.json"
+
+with open(file_path, "r") as f:
+    config = json.load(f)
+    config["extensionsGallery"]["serviceUrl"] = "https://marketplace.visualstudio.com/_apis/public/gallery"
+    config["extensionsGallery"]["cacheUrl"] = "https://vscode.blob.core.windows.net/gallery/index"
+    config["extensionsGallery"]["itemUrl"] = "https://marketplace.visualstudio.com/items"
+
+with open(file_path, "w") as f:
+    json.dump(config, f, indent=2)
