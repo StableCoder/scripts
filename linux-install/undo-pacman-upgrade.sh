07cc9baf663a65be9dab3c36e2617439bd265836
George Cave <gcave@stablecoder.ca>
2021-11-09 21:04:31 -0500

    Added script that can undo dated pacman upgrades

---
+++ linux-install/undo-pacman-upgrade.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+DATE=$1
+
+if [ -z $DATE ]; then
+    echo "ERROR: No date specified, call ./undo-pacman-upgrade.sh YYYY-MM-DD"
+    exit 1
+fi
+
+grep -a upgraded /var/log/pacman.log | grep $DATE >/tmp/lastupdates.txt
+awk '{print $4}' /tmp/lastupdates.txt >/tmp/lines1
+awk '{print $5}' /tmp/lastupdates.txt | sed 's/(/-/g' >/tmp/lines2
+paste /tmp/lines1 /tmp/lines2 >/tmp/lines
+tr -d "[:blank:]" </tmp/lines >/tmp/packages
+cd /var/cache/pacman/pkg/
+
+for i in $(cat /tmp/packages); do
+    echo "FOR: $i"
+    # sudo pacman --noconfirm -U $(ls "$i"* | grep -v -e "\.sig")
+done
